81cf32235e5b450e3596188c502282fae9b437df
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-01-14 09:55:18 +0200

    add star warz pictures

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
 import React from 'react';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
 import { StyleSheet, Text, View } from 'react-native';
 import Datafetching from './fetch/dataFetching';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Datafetching />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createStackNavigator({
+  Home: { screen: Datafetching }
 });
+
+const App = createAppContainer(navigator);
+
+export default App;
+
+
